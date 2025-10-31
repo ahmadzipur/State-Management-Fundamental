d4a902d2bf72e789f235522621e3ab19517f470b
ahmadzipur <ahmadzipur@gmail.com>
2025-10-31 18:23:02 +0700

    Upload

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class HomeScreen extends StatelessWidget {
+import '../../riverpod/hello_riverpod.dart';
+
+class HomeScreen extends HookConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Home Screen')));
+  Widget build(BuildContext context, WidgetRef ref) {
+    final counter = useState(0);
+    final hello = ref.watch(helloProvider);
+    return Scaffold(
+      body: Center(
+        child: Consumer(
+          builder: (context, ref, child) {
+            return Text('$hello ${counter.value}');
+          },
+        ),
+      ),
+    );
   }
 }
